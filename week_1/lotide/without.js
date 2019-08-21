a538b298e9854468a9c583523d824368fd4e2a6a
Anthony <anthonyzhu132@gmail.com>
2019-08-21 03:59:53 +0000

    using a without function to pick out unwanted items from array

---
+++ week_1/lotide/without.js
@@ -0,0 +1,35 @@
+const eqArrays = function(array1, array2) {
+  for (var i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if(eqArrays(array1, array2)) {
+    console.log("These arrays match");
+  } else {
+    console.log("These arrays do not match");
+  }
+}
+
+const without = function(array, itemsToRemove) {
+  let newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    let matchFound = false;
+    for (var x = 0; x < itemsToRemove.length; x++) {
+      if (array[i] === itemsToRemove[x]) {
+        matchFound = true;
+        break;
+      }
+    }
+    if (matchFound === false) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
