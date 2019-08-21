77beb39758db64047fdc34248144c2d8a46109e9
Anthony <anthonyzhu132@gmail.com>
2019-08-21 22:30:01 +0000

    Function that calculates positions at index and then pushes them into an array recording the value

---
+++ week_1/lotide/letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays = function(array1, array2) {
+  for (var i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if(eqArrays(array1, array2)) {
+    console.log("These arrays match");
+  } else {
+    console.log("These arrays do not match");
+  }
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+    for (var i = 0; i < sentence.length; i++) {
+      const letter = sentence[i];
+      if(results[letter]) {
+        results[letter].push(i)
+      } else {
+        results[letter] = [i]
+      }
+    }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+
+
+console.log(letterPositions("He ll o"));
