5e183059868a60b0ed9761f2dab5d285f0e6a1cf
Anthony <anthonyzhu132@gmail.com>
2019-08-21 23:23:27 +0000

    Using a function that passes an array and a value to compare to answer true/false

---
+++ week_1/lotide/findKeyByValue.js
@@ -0,0 +1,27 @@
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objects, value) {
+  for(let topic of Object.keys(objects)) {
+    if(objects[topic] === value) {
+      return topic;
+    } 
+    }
+};
+
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
