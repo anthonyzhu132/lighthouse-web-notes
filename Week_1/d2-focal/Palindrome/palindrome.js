21e619c4b07eef9617c935a1d2b92dcb88e4111c
Anthony <anthonyzhu132@gmail.com>
2019-08-21 01:31:43 +0000

    Debugging Palindrome.js to all pass

---
+++ Week_1/d2-focal/Palindrome/palindrome.js
@@ -0,0 +1,34 @@
+// Assertion Function
+const assertEqual = function(actual, expected, message) {
+  if (actual === expected) { 
+    console.log(`✅✅✅ Assertion Passed`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed`);
+  }
+}
+
+// FUNCTION IMPLEMENTATION (BUGGY)
+const isPalindrome = function(str) {
+  const noSpaces = str.split(" ").join("").toLowerCase();
+  const midIndex = Math.floor(noSpaces.length/2);
+  const lastIndex = noSpaces.length - 1;
+
+  for (let i = 0; i < midIndex; i++) {
+    if (noSpaces[i] !== noSpaces[lastIndex - i])
+    return false;
+  }
+  return true;
+}
+
+// TEST CODE
+// These should all pass assertion, but they don't. 
+assertEqual(isPalindrome('p'), true);
+assertEqual(isPalindrome('p'), true);
+assertEqual(isPalindrome('foo'), false);
+assertEqual(isPalindrome('racecar'), true);
+assertEqual(isPalindrome('live without evil'), false);
+assertEqual(isPalindrome('just some random words'), false);
+
+// Bonus / Stretch: Uncomment these tests and figure out why these are also failing
+assertEqual(isPalindrome('Kayak'), true);
+assertEqual(isPalindrome('a santa at NASA'), true);
