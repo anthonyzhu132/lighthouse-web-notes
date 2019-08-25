5b1fd0555c6f30d4213fba9aa742e650724f9e3b
Anthony <anthonyzhu132@gmail.com>
2019-08-25 03:40:34 +0000

    updated functionality to work between all lotide functions

--- week_1/lotide/assertArraysEqual.js
+++ week_1/lotide/assertArraysEqual.js
@@ -1,18 +1,18 @@
 const eqArrays = function(array1, array2) {
-  for (var i = 0; i < array1.length; i++) {
+  for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   }
   return true;
-}
+};
 
 const assertArraysEqual = function(array1, array2) {
-  if(eqArrays(array1, array2)) {
+  if (eqArrays(array1, array2)) {
     console.log("These arrays match");
   } else {
     console.log("These arrays do not match");
   }
-}
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
