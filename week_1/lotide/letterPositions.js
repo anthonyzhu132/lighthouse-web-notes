83c9a812eb59e631c4f81aadbbc89ead9edb2d64
Anthony <anthonyzhu132@gmail.com>
2019-08-25 03:31:10 +0000

    finds letter positions of a string and verifies if statement is true

--- week_1/lotide/letterPositions.js
+++ week_1/lotide/letterPositions.js
@@ -5,15 +5,15 @@ const eqArrays = function(array1, array2) {
     } 
   }
   return true;
-}
+};
 
 const assertArraysEqual = function(array1, array2) {
-  if(eqArrays(array1, array2)) {
+  if (eqArrays(array1, array2)) {
     console.log("These arrays match");
   } else {
     console.log("These arrays do not match");
   }
-}
+};
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -23,15 +23,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// LETTER POSITIONS //
 const letterPositions = function(sentence) {
   const results = {};
     for (var i = 0; i < sentence.length; i++) {
-      const letter = sentence[i];
-      if(results[letter]) {
-        results[letter].push(i)
+    const letter = sentence[i];
+    if (sentence[i] !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
       } else {
-        results[letter] = [i]
+        results[letter] = [i];
       }
+    }
+    // eslint-disable-next-line indent
     }
   return results;
 };
