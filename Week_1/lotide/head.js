a7ad8cb502421928ff6f111fc6bcf88aabd563ea
Anthony <anthonyzhu132@gmail.com>
2019-08-26 19:52:13 +0000

    refactored head.js

--- Week_1/lotide/head.js
+++ Week_1/lotide/head.js
@@ -1,12 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ❌❌ ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   x = array[0];
   return x;
-}
+};
+
+module.exports = head;
