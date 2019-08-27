bde38200b109079b9202d2c2bb967da547b28af3
Anthony <anthonyzhu132@gmail.com>
2019-08-27 23:58:39 +0000

    updated with finishing paragraph

--- week_2/d2_async.flow/profile_generator/survey.js
+++ week_2/d2_async.flow/profile_generator/survey.js
@@ -5,20 +5,21 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What is your nickname? ', (answer) => {
-  console.log(`Cool Name! ${answer}`);
-  rl.question('What is an activity you like doing?', (answer) => {
+rl.question('What is your nickname? ', (answer1) => {
+  console.log(`Cool Name! ${answer1}`);
+  rl.question('What is an activity you like doing?', (answer2) => {
     console.log(`Cool`);
-    rl.question('What do you listen to while doing that? ', (answer) => {
+    rl.question('What do you listen to while doing that? ', (answer3) => {
       console.log(`Cool`);
-      rl.question('Which meal is your favourite ', (answer) => {
+      rl.question('Which meal is your favourite ', (answer4) => {
         console.log(`Cool`);
-        rl.question('Whats your favourite thing to eat for that meal? ', (answer) => {
+        rl.question('Whats your favourite thing to eat for that meal? ', (answer5) => {
           console.log(`Cool`);
-          rl.question('Which sport is your absolute favourite? ', (answer) => {
+          rl.question('Which sport is your absolute favourite? ', (answer6) => {
             console.log(`Cool`);
-            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer7) => {
               console.log(`Cool`);
+              console.log(answer1 + " Loves Listening to " + answer3 + " While " + answer2 + " devouring " + answer5 + " for " + answer4 + " prefers " + answer6 + " oveer any sport" + " and is great at " + answer7);
               rl.close();
             });
           });
@@ -27,3 +28,4 @@ rl.question('What is your nickname? ', (answer) => {
     });
   });
 });
+
