0bd35a922eb3dd53699564ab66c294670db480ed
Anthony <anthonyzhu132@gmail.com>
2019-08-27 22:27:38 +0000

    updated with if conditions

--- week_2/d2_async.flow/timer1.js
+++ week_2/d2_async.flow/timer1.js
@@ -1,17 +1,21 @@
 const args = process.argv.slice(2);
 
 // for (let i = 0; i < args.length; i++) {
-//   const timer = function (args) {
-//     setTimeout(() => {
-//       console.log("Hello");
-//       process.stdout.write('\x07');
-//     }, args[i] * 1000);
-//   };
+//   setTimeout(() => {
+//     // console.log("Hello");
+//     process.stdout.write('\x07');
+//   }, args[i] * 1000);
 // }
 
 for (let i = 0; i < args.length; i++) {
-  setTimeout(() => {
-    // console.log("Hello");
-    process.stdout.write('\x07');
-  }, args[i] * 1000);
+  if (args[i] < 0) {
+    // console.log("Bye");
+  } else if (isNaN(args[i])) {
+    // console.log("Not a number");
+  } else {
+    setTimeout(() => {
+      // console.log("Hello");
+      process.stdout.write('\x07');
+    }, args[i] * 1000);
+  }
 }
