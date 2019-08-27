e19d7b11fc5f679f381f298e48d0da8039f1f754
Anthony <anthonyzhu132@gmail.com>
2019-08-27 05:37:22 +0000

    using module export to call functions from other files and importing files using require, refactoring the following

--- week_1/lotide/eqArrays.js
+++ week_1/lotide/eqArrays.js
@@ -1,19 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ❌❌ ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require("./assertEqual");
 
 const eqArrays = function(array1, array2) {
-  for (var i = 0; i < array1.length; i++) {
+  for (let i =  0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   }
   return true;
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
+
--- week_1/lotide/middle.js
+++ week_1/lotide/middle.js
@@ -1,19 +1,5 @@
-const eqArrays = function(array1, array2) {
-  for (var i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } 
-  }
-  return true;
-}
-
-const assertArraysEqual = function(array1, array2) {
-  if(eqArrays(array1, array2)) {
-    console.log("These arrays match");
-  } else {
-    console.log("These arrays do not match");
-  }
-}
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function(array) {
   let middleArray = [];
@@ -31,4 +17,4 @@ const middle = function(array) {
   return middleArray;
 };
 
-console.log(middle([1, 2, 3]));
+module.exports = middle;
