ed7910a9312e2b54a9a590fe66d09ebfdee9dcee
Anthony <anthonyzhu132@gmail.com>
2019-08-27 05:33:53 +0000

    created test functions for head, tail and assertArrayEqual using mocha & chai

--- Week_1/lotide/test/tailTest.js
+++ Week_1/lotide/test/tailTest.js
@@ -1,7 +1,18 @@
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require('chai').assert;
+
+describe("#Tail", () => {
+  it("returns [2, 3,]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns [2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+});
 
 // Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
