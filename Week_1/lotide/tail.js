a20ad43246d433e31446024207f13913e6cfa936
Anthony <anthonyzhu132@gmail.com>
2019-08-27 05:39:55 +0000

    refactored functions, shortened code and used functions to call other functions with module export and require()

--- Week_1/lotide/tail.js
+++ Week_1/lotide/tail.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ❌❌ ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function(array) {
-  x = array.slice(1);
-}
+  let x = array.slice(1);
+  return x;
+};
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
